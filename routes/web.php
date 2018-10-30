0c56f00bd2ee551376c0c3462ad7341eb1466c36
William Alfonso <wwallas@gmail.com>
2018-10-30 10:36:59 -0600

    changes

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,12 @@
 |
 */
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
 // Route::get('/feed', function () {
 //     return view('feed');
 // });
-Route::get(' ', 'apiController@getdata');
-=======
+// Route::get(' ', 'apiController@getdata');
+
 Route::get('/', function () {
     return view('index');
     //return view('autocomplete');
@@ -50,4 +50,3 @@ Route::get('/profile/{techid}/create', 'ProfileWebtecheController@create');
 // Auth::routes();
 //
 // Route::get('/home', 'HomeController@index')->name('home');
->>>>>>> 81247729b51916f453f55ccb26e08a0863d1b473
