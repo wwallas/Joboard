3f7e305a407128513b7bae05e08bcd3e6db194d2
William Alfonso <wwallas@gmail.com>
2018-10-31 10:43:42 -0600

    file profile and web

--- app/Http/Controllers/ProfileWebtecheController.php
+++ app/Http/Controllers/ProfileWebtecheController.php
@@ -24,26 +24,13 @@ class ProfileWebtecheController extends Controller
         $user = User::find($id);
         // dd($user->profile->technology);
         $techs = $user->profile->technology;
-        dd($techs);
-
-
-
+        
         $user->technologies()->attach($techid);
-
-        //$user->technologies()->syncWithoutDetaching([$techid->id]);
-        // if (! $user->technologies->contains($techid->id)) {
-        //         $user->technologies()->save($techid);
-        //
-            //dd($data);
-            //set the $follower variable, so the ID of our new row is accesible there
-            $addtech = ProfileWebteche::create($data);
-            // dd($addtech);
-
-
-        }
-
-
         return back();
+
+
+
+
 
         // return view('profile.profileupdate', compact('techs'))->with('user', $user);
         // return redirect('invoice/create/'.$invoice_id)->with($invoice_id);
@@ -54,19 +41,9 @@ class ProfileWebtecheController extends Controller
         $user_id= auth()->user()->id;
         $user = User::find($user_id);
 
-        
-        dd($user->Technology);
-// dd($user);
-        // $technos = DB::table('profiles')
-        //     ->select('Technologys.description')
-        //     ->join('profiles', 'user.id', '=', 'Technologys.user_id')
-        //     ->where('Technologys.user_id', $user_id)
-        //     ->orderby ('Technologys.id')->get();
 
-            // $technos = DB::table('profiles')
-            // ->select('Technologys.description')
-            // ->join('Technologys', 'profiles.user_id', '=', 'Technologys.user_id')
-            // ->get();
+        // dd($user->Technology);
+        return view('profile.index',compose($user->Technology));
 
         // dd($followers);
         //return view('profile.index')->with('technos', $technos, $user->profile_webteches);
