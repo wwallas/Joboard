eac90e3acb4e59ecd64a8f3c8840580713b1f23e
William Alfonso <wwallas@gmail.com>
2018-10-31 10:23:27 -0600

    file profile

--- app/Http/Controllers/ProfileWebtecheController.php
+++ app/Http/Controllers/ProfileWebtecheController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\User;
 use App\Profile;
 use App\Technology;
-use App\ProfileWebteche;
+use App\TechnologyUser;
 
 use Auth;
 
@@ -23,29 +23,18 @@ class ProfileWebtecheController extends Controller
         $id= auth()->user()->id;
         $user = User::find($id);
         // dd($user->profile->technology);
-        $techs = $user->profile->technology;
-        dd($techs);
+        // dd($techid);
+        // $techs = $user->technology;
 
-        if($techs == null){
-            $data['user_id']= auth()->user()->id;
-            $data['Technology_id']= $techid;
+        // $matchThese = ['technology_id' => $techid, 'user_id' => $user];
+        // $techs= TechnologyUser::where($matchThese)->count();
 
-<<<<<<< HEAD
         $user->technologies()->attach($techid);
 
         //$user->technologies()->syncWithoutDetaching([$techid->id]);
         // if (! $user->technologies->contains($techid->id)) {
         //         $user->technologies()->save($techid);
         //     }
-=======
-            //dd($data);
-            //set the $follower variable, so the ID of our new row is accesible there
-            $addtech = ProfileWebteche::create($data);
-            // dd($addtech);
-
-
-        }
->>>>>>> f53a88044a5f7f2d00b5ca2f47e7b209ddb6ca48
 
         return back();
 
@@ -58,7 +47,7 @@ class ProfileWebtecheController extends Controller
         $user_id= auth()->user()->id;
         $user = User::find($user_id);
 
-        $user = Profile::find($user)->where;
+        $user = Profile::find($user);
 
         dd($user->Technology);
 // dd($user);
