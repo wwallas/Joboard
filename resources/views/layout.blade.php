8363c7ac305741f54c3a9fe2ddb0a9ebd77f9702
Ryan Dionne <jryand1030@gmail.com>
2018-11-01 14:24:11 -0600

    update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,7 +16,7 @@
     <!-- <link href="/css/bootstrap.min.css" rel="stylesheet"> -->
     <link href="/css/stylesheet.css" rel="stylesheet">
 
-    <!-- <link rel="stylesheet" href="/css/style.css"> -->
+    <link rel="stylesheet" href="/css/style.css">
     <!-- Custom styles for this template -->
     <link href="/css/navbar-top-fixed.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
